ecf63da8d3cd8144240fc9833d0fb65641fe595b
rodrigodsluz <rodrigodsluz@gmail.com>
2020-07-18 11:38:40 -0300

    [ADD] Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Class-based component
+class App extends React.Component{
+  //render returns a React component to be rendered
+  render(){
+    return(
+      <div>
+        <h1>App</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
