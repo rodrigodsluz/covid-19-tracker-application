a0152a455a66e591d4d5292ef110d41b0eed0831
rodrigodsluz <rodrigodsluz@gmail.com>
2020-07-19 11:48:41 -0300

    Add Material UI

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,9 +1,20 @@
   import React from 'react'
+  import { Card, CardContent, Typography, Grid, StylesProvider} from '@material-ui/core'
+
+  import styles from './Cards.module.css'
 
   const Cards = (props) => {
     console.log(props)
     return (
-      <h1>Cards</h1>
+      <div className={styles.container}>
+        <Grid container spacing={3} justify="center" >
+          <Grid item component={Card} >
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </div>
     )
   }
 
